4ddf803ed471f560cc63238841ab644f385cf1cf
Henry Schumacher <94359873+SirPhoenix007@users.noreply.github.com>
2025-08-06 14:09:14 +0200

    Spectra Update
    added functionality to the spectra plotter

---
+++ 1_RootToPythonConverter/colors.py
@@ -0,0 +1,7 @@
+def load_colors():
+    # Colorschemes
+    c_rainbow =         ['#690000','#C1292E','#e08e45','#E0BE36','#7A8C2A','#0D863D','#2d82b7','#740091']
+    c_complementary =   ['#D90368','#D4C200','#2CDA9D','#BA3B46','#FA824C','#3C91E6','#009528','#6689C6']
+    c_violetorange =    ['#5E0ABF','#701AA4','#82298A','#94396F','#A64854','#B85839','#CA671F','#DC7704']
+    color_dictionary = {'c_rainbow': c_rainbow, 'c_complementary':c_complementary, 'c_violetorange':c_violetorange}
+    return color_dictionary
