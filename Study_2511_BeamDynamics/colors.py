3fa8de9886143dd0bcc0f907ed1899b8adcf36c4
Henry Schumacher <94359873+SirPhoenix007@users.noreply.github.com>
2025-11-07 11:46:00 +0100

    Introduced Study 2511

---
+++ Study_2511_BeamDynamics/colors.py
@@ -0,0 +1,8 @@
+def load_colors():
+    # Colorschemes
+    c_rainbow =         ['#690000','#C1292E','#e08e45','#E0BE36','#7A8C2A','#0D863D','#2d82b7','#740091']
+    c_complementary =   ['#D90368','#4C5395','#2CDA9D','#FA824C','#3C91E6','#FFBA08','#009528','#6689C6'] #'#BA3B46'
+    c_violetorange =    ['#5E0ABF','#701AA4','#82298A','#94396F','#A64854','#B85839','#CA671F','#DC7704']
+    c_five =            ['#FFBA08','#E05A5F','#3D405B','#81B29A', '#C45AB3']
+    color_dictionary = {'c_rainbow': c_rainbow, 'c_complementary':c_complementary, 'c_violetorange':c_violetorange, 'c_five':c_five}
+    return color_dictionary
